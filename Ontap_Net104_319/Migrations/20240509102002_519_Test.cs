c0ee903401585abfb166a1f91c4accd760befe6d
shanghaik168@gmail.com <Shanghaik1998>
2024-05-09 18:14:51 +0700

    Add project files.

---
+++ Ontap_Net104_319/Migrations/20240509102002_519_Test.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ontap_Net104_319.Migrations
+{
+    public partial class _519_Test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    Username = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    MatKhau = table.Column<string>(type: "varchar(256)", maxLength: 256, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nchar(256)", fixedLength: true, maxLength: 256, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.Username);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accounts");
+        }
+    }
+}
---
+++ Ontap_Net104_319/Models/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Ontap_Net104_319.Models
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(){} // constructor ko tham số của nó
+        public AppDbContext(DbContextOptions options) : base(options) // constructor kế thừa
+        {
+        }
+        public DbSet<Account> Accounts { get; set; } // Đại diện cho 1 bảng trong CSDL
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=SHANGHAIK;Initial Catalog=Ontap319;Integrated Security=True; TrustServerCertificate=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
